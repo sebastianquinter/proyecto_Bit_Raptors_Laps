b1a36b5fd89650fad4ee99951f0195857df7744d
Sebastian Quintero <Sebastian Quintero@DESKTOP-H37M543>
2026-07-05 12:20:56 -0500

    Reorganiza archivos CSS y actualiza configuración de estáticos

--- proyectofreddy/settings.py
+++ proyectofreddy/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "inseguro-local")
 
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
     "bitraptorlabs.com",
